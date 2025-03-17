dca6cf5e9099c748fe3ec4771d356f4abbf4ba54
vanipm <waveam.vp@gmail.com>
2025-03-17 18:20:32 -0400

    Update EncodedFrame.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/EncodedFrame.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/EncodedFrame.java
@@ -13,23 +13,112 @@ See the License for the specific language governing permissions and limitations 
 */
 package com.amazonaws.kinesisvideo.parser.examples.lambda;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.nio.ByteBuffer;
 
-/**
- * Container class for H264 encoded frame
- */
-@Getter
-@Builder
 public class EncodedFrame {
-
     private final ByteBuffer byteBuffer;
     private final ByteBuffer cpd;
     private final boolean isKeyFrame;
-    @Setter
     private long timeCode;
+    long producerSideTimeStampMillis;
+    long serverSideTimeStampMillis;
 
+    EncodedFrame(ByteBuffer byteBuffer, ByteBuffer cpd, boolean isKeyFrame, long timeCode, long producerSideTimeStampMillis, long serverSideTimeStampMillis) {
+        this.byteBuffer = byteBuffer;
+        this.cpd = cpd;
+        this.isKeyFrame = isKeyFrame;
+        this.timeCode = timeCode;
+        this.producerSideTimeStampMillis = producerSideTimeStampMillis;
+        this.serverSideTimeStampMillis = serverSideTimeStampMillis;
+    }
+
+    public static EncodedFrameBuilder builder() {
+        return new EncodedFrameBuilder();
+    }
+
+    public ByteBuffer getByteBuffer() {
+        return this.byteBuffer;
+    }
+
+    public ByteBuffer getCpd() {
+        return this.cpd;
+    }
+
+    public boolean isKeyFrame() {
+        return this.isKeyFrame;
+    }
+
+    public long getTimeCode() {
+        return this.timeCode;
+    }
+
+    public long getProducerSideTimeStampMillis() {
+        return this.producerSideTimeStampMillis;
+    }
+
+    public long getServerSideTimeStampMillis() {
+        return this.serverSideTimeStampMillis;
+    }
+
+    public void setTimeCode(long timeCode) {
+        this.timeCode = timeCode;
+    }
+
+    public void setProducerSideTimeStampMillis(long producerSideTimeStampMillis) {
+        this.producerSideTimeStampMillis = producerSideTimeStampMillis;
+    }
+
+    public void setServerSideTimeStampMillis(long serverSideTimeStampMillis) {
+        this.serverSideTimeStampMillis = serverSideTimeStampMillis;
+    }
+
+    public static class EncodedFrameBuilder {
+        private ByteBuffer byteBuffer;
+        private ByteBuffer cpd;
+        private boolean isKeyFrame;
+        private long timeCode;
+        private long producerSideTimeStampMillis;
+        private long serverSideTimeStampMillis;
+
+        EncodedFrameBuilder() {
+        }
+
+        public EncodedFrameBuilder byteBuffer(ByteBuffer byteBuffer) {
+            this.byteBuffer = byteBuffer;
+            return this;
+        }
+
+        public EncodedFrameBuilder cpd(ByteBuffer cpd) {
+            this.cpd = cpd;
+            return this;
+        }
+
+        public EncodedFrameBuilder isKeyFrame(boolean isKeyFrame) {
+            this.isKeyFrame = isKeyFrame;
+            return this;
+        }
+
+        public EncodedFrameBuilder timeCode(long timeCode) {
+            this.timeCode = timeCode;
+            return this;
+        }
+
+        public EncodedFrameBuilder producerSideTimeStampMillis(long producerSideTimeStampMillis) {
+            this.producerSideTimeStampMillis = producerSideTimeStampMillis;
+            return this;
+        }
+
+        public EncodedFrameBuilder serverSideTimeStampMillis(long serverSideTimeStampMillis) {
+            this.serverSideTimeStampMillis = serverSideTimeStampMillis;
+            return this;
+        }
+
+        public EncodedFrame build() {
+            return new EncodedFrame(this.byteBuffer, this.cpd, this.isKeyFrame, this.timeCode, this.producerSideTimeStampMillis, this.serverSideTimeStampMillis);
+        }
+
+        public String toString() {
+            return "EncodedFrame.EncodedFrameBuilder(byteBuffer=" + this.byteBuffer + ", cpd=" + this.cpd + ", isKeyFrame=" + this.isKeyFrame + ", timeCode=" + this.timeCode + ", producerSideTimeStampMillis=" + this.producerSideTimeStampMillis + ", serverSideTimeStampMillis=" + this.serverSideTimeStampMillis + ")";
+        }
+    }
 }
