3e6cb68c88359f2616e83381cdf71835a3a36f08
vanipm <waveam.vp@gmail.com>
2025-03-17 18:22:31 -0400

    Update KVSMediaSource.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/KVSMediaSource.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/KVSMediaSource.java
@@ -11,6 +11,7 @@ or in the "license" file accompanying this file.
 This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
+
 package com.amazonaws.kinesisvideo.parser.examples.lambda;
 
 import com.amazonaws.kinesisvideo.client.mediasource.CameraMediaSourceConfiguration;
@@ -19,120 +20,100 @@ import com.amazonaws.kinesisvideo.common.exception.KinesisVideoException;
 import com.amazonaws.kinesisvideo.internal.client.mediasource.MediaSource;
 import com.amazonaws.kinesisvideo.internal.client.mediasource.MediaSourceConfiguration;
 import com.amazonaws.kinesisvideo.internal.client.mediasource.MediaSourceSink;
-import com.amazonaws.kinesisvideo.parser.utilities.ProducerStreamUtil;
 import com.amazonaws.kinesisvideo.producer.KinesisVideoFrame;
 import com.amazonaws.kinesisvideo.producer.StreamCallbacks;
 import com.amazonaws.kinesisvideo.producer.StreamInfo;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import javax.annotation.Nullable;
-import java.nio.ByteBuffer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
-@RequiredArgsConstructor
 public class KVSMediaSource implements MediaSource {
-
+    private static final Logger log = LoggerFactory.getLogger(KVSMediaSource.class);
     private static final int FRAME_FLAG_KEY_FRAME = 1;
     private static final int FRAME_FLAG_NONE = 0;
-    private static final long HUNDREDS_OF_NANOS_IN_MS = 10 * 1000;
+    private static final long HUNDREDS_OF_NANOS_IN_MS = 10000L;
     private static final long FRAME_DURATION_20_MS = 20L;
-    
     private CameraMediaSourceConfiguration cameraMediaSourceConfiguration;
     private MediaSourceState mediaSourceState;
     private MediaSourceSink mediaSourceSink;
     private int frameIndex;
     private final StreamInfo streamInfo;
 
-    private void putFrame(final KinesisVideoFrame kinesisVideoFrame) {
+    private void putFrame(KinesisVideoFrame kinesisVideoFrame) {
         try {
-           log.debug("PutFrame Sink for frame no : {}", kinesisVideoFrame);
-            mediaSourceSink.onFrame(kinesisVideoFrame);
-        } catch (final KinesisVideoException ex) {
+            log.debug("PutFrame for frame no : {}", this.frameIndex);
+            this.mediaSourceSink.onFrame(kinesisVideoFrame);
+        } catch (KinesisVideoException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    @Override
     public MediaSourceState getMediaSourceState() {
-        return mediaSourceState;
+        return this.mediaSourceState;
     }
 
-    @Override
     public MediaSourceConfiguration getConfiguration() {
-        return cameraMediaSourceConfiguration;
+        return this.cameraMediaSourceConfiguration;
     }
 
-    @Override
     public StreamInfo getStreamInfo() throws KinesisVideoException {
-        return streamInfo;
+        return this.streamInfo;
     }
 
-    @Override
-    public void initialize(final MediaSourceSink mediaSourceSink) {
+    public void initialize(MediaSourceSink mediaSourceSink) {
         this.mediaSourceSink = mediaSourceSink;
     }
 
-    @Override
-    public void configure(final MediaSourceConfiguration configuration) {
-
+    public void configure(MediaSourceConfiguration configuration) {
         if (!(configuration instanceof CameraMediaSourceConfiguration)) {
             throw new IllegalStateException("Configuration must be an instance of CameraMediaSourceConfiguration");
+        } else {
+            this.cameraMediaSourceConfiguration = (CameraMediaSourceConfiguration)configuration;
+            this.frameIndex = 0;
         }
-        this.cameraMediaSourceConfiguration = (CameraMediaSourceConfiguration) configuration;
-        this.frameIndex = 0;
-        
     }
 
-    @Override
     public void start() {
-        mediaSourceState = MediaSourceState.RUNNING;
+        this.mediaSourceState = MediaSourceState.RUNNING;
     }
 
-    public void putFrameData(final EncodedFrame encodedFrame) {
-       log.debug("putFrameData : {}", encodedFrame);
-       final int flags = encodedFrame.isKeyFrame() ? FRAME_FLAG_KEY_FRAME : FRAME_FLAG_NONE;
+    public void putFrameData(EncodedFrame encodedFrame) {
+        log.debug("putFrameData : {} producerSideTimeStampMillis {} serverSideTimeStampMillis {} ", new Object[]{encodedFrame, encodedFrame.getProducerSideTimeStampMillis(), encodedFrame.getServerSideTimeStampMillis()});
+        int flags = encodedFrame.isKeyFrame() ? 1 : 0;
         if (encodedFrame.getByteBuffer() != null) {
-            final KinesisVideoFrame frame = new KinesisVideoFrame(
-                    frameIndex++,
-                    flags,
-                    encodedFrame.getTimeCode() * HUNDREDS_OF_NANOS_IN_MS,
-                    encodedFrame.getTimeCode() * HUNDREDS_OF_NANOS_IN_MS,
-                    FRAME_DURATION_20_MS * HUNDREDS_OF_NANOS_IN_MS,
-                    encodedFrame.getByteBuffer());
+            KinesisVideoFrame frame = new KinesisVideoFrame(this.frameIndex++, flags, encodedFrame.getTimeCode(), encodedFrame.getProducerSideTimeStampMillis(), 20L, encodedFrame.getByteBuffer());
             if (frame.getSize() == 0) {
                 return;
             }
-            putFrame(frame);
+
+            this.putFrame(frame);
         } else {
             log.info("Frame Data is null !");
         }
+
     }
 
-    @Override
     public void stop() {
-        mediaSourceState = MediaSourceState.STOPPED;
-        
+        this.mediaSourceState = MediaSourceState.STOPPED;
     }
 
-    @Override
     public boolean isStopped() {
-        return mediaSourceState == MediaSourceState.STOPPED;
+        return this.mediaSourceState == MediaSourceState.STOPPED;
     }
 
-    @Override
     public void free() {
-
     }
 
-    @Override
     public MediaSourceSink getMediaSourceSink() {
-        return mediaSourceSink;
+        return this.mediaSourceSink;
     }
 
     @Nullable
-    @Override
     public StreamCallbacks getStreamCallbacks() {
         return null;
+    }
+
+    public KVSMediaSource(StreamInfo streamInfo) {
+        this.streamInfo = streamInfo;
     }
 }
