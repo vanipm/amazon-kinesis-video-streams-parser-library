43d3d12e4c9ef2340f4c9930acc27ad8b2e35492
vanipm <waveam.vp@gmail.com>
2025-03-17 00:33:42 -0400

    Update GetMediaForFragmentListWorker.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/GetMediaForFragmentListWorker.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/GetMediaForFragmentListWorker.java
@@ -11,8 +11,8 @@ or in the "license" file accompanying this file.
 This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-package com.amazonaws.kinesisvideo.parser.examples;
 
+package com.amazonaws.kinesisvideo.parser.examples;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -24,71 +24,54 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.kinesisvideo.AmazonKinesisVideo;
 import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoArchivedMedia;
 import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoArchivedMediaClient;
+import com.amazonaws.services.kinesisvideo.AmazonKinesisVideoArchivedMediaClientBuilder;
 import com.amazonaws.services.kinesisvideo.model.APIName;
 import com.amazonaws.services.kinesisvideo.model.GetDataEndpointRequest;
 import com.amazonaws.services.kinesisvideo.model.GetMediaForFragmentListRequest;
 import com.amazonaws.services.kinesisvideo.model.GetMediaForFragmentListResult;
-import lombok.extern.slf4j.Slf4j;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@Slf4j
 public class GetMediaForFragmentListWorker extends KinesisVideoCommon implements Runnable {
+    private static final Logger log = LoggerFactory.getLogger(GetMediaForFragmentListWorker.class);
     private final AmazonKinesisVideoArchivedMedia amazonKinesisVideoArchivedMedia;
     private final MkvElementVisitor elementVisitor;
     private final List<String> fragmentNumbers;
 
-    public GetMediaForFragmentListWorker(final String streamName, final List<String> fragmentNumbers,
-                                         final AWSCredentialsProvider awsCredentialsProvider, final String endPoint,
-                                         final Regions region, final MkvElementVisitor elementVisitor) {
+    public GetMediaForFragmentListWorker(String streamName, List<String> fragmentNumbers, AWSCredentialsProvider awsCredentialsProvider, String endPoint, Regions region, MkvElementVisitor elementVisitor) {
         super(region, awsCredentialsProvider, streamName);
         this.fragmentNumbers = fragmentNumbers;
         this.elementVisitor = elementVisitor;
-        amazonKinesisVideoArchivedMedia = AmazonKinesisVideoArchivedMediaClient
-                .builder()
-                .withCredentials(awsCredentialsProvider)
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region.getName()))
-                .build();
+        this.amazonKinesisVideoArchivedMedia = (AmazonKinesisVideoArchivedMedia)((AmazonKinesisVideoArchivedMediaClientBuilder)((AmazonKinesisVideoArchivedMediaClientBuilder)AmazonKinesisVideoArchivedMediaClient.builder().withCredentials(awsCredentialsProvider)).withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, region.getName()))).build();
     }
 
-
-    public static GetMediaForFragmentListWorker create(final String streamName, final List<String> fragmentNumbers,
-                                                       final AWSCredentialsProvider awsCredentialsProvider,
-                                                       final Regions region,
-                                                       final AmazonKinesisVideo amazonKinesisVideo,
-                                                       final MkvElementVisitor elementVisitor) {
-        final GetDataEndpointRequest request = new GetDataEndpointRequest()
-                .withAPIName(APIName.GET_MEDIA_FOR_FRAGMENT_LIST).withStreamName(streamName);
-        final String endpoint = amazonKinesisVideo.getDataEndpoint(request).getDataEndpoint();
-        return new GetMediaForFragmentListWorker(
-                streamName, fragmentNumbers, awsCredentialsProvider, endpoint, region, elementVisitor);
+    public static GetMediaForFragmentListWorker create(String streamName, List<String> fragmentNumbers, AWSCredentialsProvider awsCredentialsProvider, Regions region, AmazonKinesisVideo amazonKinesisVideo, MkvElementVisitor elementVisitor) {
+        GetDataEndpointRequest request = (new GetDataEndpointRequest()).withAPIName(APIName.GET_MEDIA_FOR_FRAGMENT_LIST).withStreamName(streamName);
+        String endpoint = amazonKinesisVideo.getDataEndpoint(request).getDataEndpoint();
+        return new GetMediaForFragmentListWorker(streamName, fragmentNumbers, awsCredentialsProvider, endpoint, region, elementVisitor);
     }
 
-    @Override
     public void run() {
         try {
-            log.info("Start GetMediaForFragmentList worker on stream {}", streamName);
-            final GetMediaForFragmentListResult result = amazonKinesisVideoArchivedMedia.getMediaForFragmentList(
-                    new GetMediaForFragmentListRequest()
-                            .withFragments(fragmentNumbers)
-                            .withStreamName(streamName));
+            log.info("Start GetMediaForFragmentList worker on stream {}", this.streamName);
+            GetMediaForFragmentListResult result = this.amazonKinesisVideoArchivedMedia.getMediaForFragmentList((new GetMediaForFragmentListRequest()).withFragments(this.fragmentNumbers).withStreamName(this.streamName));
+            log.info("GetMediaForFragmentList called on stream {} response {} requestId {}", new Object[]{this.streamName, result.getSdkHttpMetadata().getHttpStatusCode(), result.getSdkResponseMetadata().getRequestId()});
+            StreamingMkvReader mkvStreamReader = StreamingMkvReader.createDefault(new InputStreamParserByteSource(result.getPayload()));
+            log.info("StreamingMkvReader created for stream {} ", this.streamName);
 
-            log.info("GetMediaForFragmentList called on stream {} response {} requestId {}",
-                    streamName,
-                    result.getSdkHttpMetadata().getHttpStatusCode(),
-                    result.getSdkResponseMetadata().getRequestId());
-            final StreamingMkvReader mkvStreamReader = StreamingMkvReader.createDefault(
-                    new InputStreamParserByteSource(result.getPayload()));
-            log.info("StreamingMkvReader created for stream {} ", streamName);
             try {
                 mkvStreamReader.apply(this.elementVisitor);
-            } catch (final MkvElementVisitException e) {
+            } catch (MkvElementVisitException e) {
                 log.error("Exception while accepting visitor {}", e);
             }
-        } catch (final Throwable t) {
-            log.error("Failure in GetMediaForFragmentListWorker for streamName {} {}", streamName, t);
+        } catch (Throwable t) {
+            log.error("Failure in GetMediaForFragmentListWorker for streamName {} {}", this.streamName, t);
             throw t;
         } finally {
-            log.info("Exiting GetMediaWorker for stream {}", streamName);
+            log.info("Exiting GetMediaWorker for stream {}", this.streamName);
         }
+
     }
 }
+
