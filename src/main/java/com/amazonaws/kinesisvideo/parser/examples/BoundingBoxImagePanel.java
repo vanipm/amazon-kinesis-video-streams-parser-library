3bfe7e924081344b43207c0a13a3afcfa885d94c
vanipm <waveam.vp@gmail.com>
2025-03-17 00:15:18 -0400

    Update BoundingBoxImagePanel.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/BoundingBoxImagePanel.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/BoundingBoxImagePanel.java
@@ -11,110 +11,110 @@ or in the "license" file accompanying this file.
 This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-package com.amazonaws.kinesisvideo.parser.examples;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+
+package com.amazonaws.kinesisvideo.parser.examples;
 
 import com.amazonaws.kinesisvideo.parser.rekognition.pojo.BoundingBox;
 import com.amazonaws.kinesisvideo.parser.rekognition.pojo.FaceType;
 import com.amazonaws.kinesisvideo.parser.rekognition.pojo.MatchedFace;
 import com.amazonaws.kinesisvideo.parser.rekognition.pojo.RekognizedOutput;
-import lombok.extern.slf4j.Slf4j;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Panel which is used for rendering frames and embedding bounding boxes on the frames.
- */
-@Slf4j
 public class BoundingBoxImagePanel extends ImagePanel {
+    private static final Logger log = LoggerFactory.getLogger(BoundingBoxImagePanel.class);
     private static final String DELIMITER = "-";
 
-    @Override
-    public void paintComponent(final Graphics g) {
+    public BoundingBoxImagePanel() {
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
     }
 
-    public void processRekognitionOutput(final Graphics2D g2, final int width, final int height,
-                                          final RekognizedOutput rekognizedOutput) {
-        if (rekognizedOutput != null) {
+    public void processRekognitionOutput(Graphics2D g2, int width, int height, RekognizedOutput rekognizedOutput) {
+        if (rekognizedOutput != null && rekognizedOutput.getFaceSearchOutputs() != null) {
+            log.debug("Number of detected faces in a frame {}", rekognizedOutput.getFaceSearchOutputs().size());
 
-            // Draw bounding boxes for faces.
-            if (rekognizedOutput.getFaceSearchOutputs() != null) {
-                log.debug("Number of detected faces in a frame {}", rekognizedOutput.getFaceSearchOutputs().size());
-                for (final RekognizedOutput.FaceSearchOutput faceSearchOutput : rekognizedOutput.getFaceSearchOutputs()) {
-                    final FaceType detectedFaceType;
-                    final String title;
-                    if (!faceSearchOutput.getMatchedFaceList().isEmpty()) {
-                        // Taking First match as Rekognition returns set of matched faces sorted by confidence level
-                        final MatchedFace matchedFace = faceSearchOutput.getMatchedFaceList().get(0);
-                        final String externalImageId = matchedFace.getFace().getExternalImageId();
-                        // Rekognition doesn't allow any extra attributes/tags to be associated with the 'Face'.
-                        // External Image Id is used here to draw title on top of the bounding box and change color
-                        // of the bounding box (based on the FaceType). External Image Id needs to be specified in
-                        // below format in order get this working.
-                        // Eg: PersonName1-Criminal, PersonName2-Trusted, PersonName3-Intruder etc.
-                        if (externalImageId == null) {
-                            // If the external image id is not specified, then draw confidence level as title.
-                            title = matchedFace.getFace().getConfidence() + "";
-                            detectedFaceType = FaceType.NOT_RECOGNIZED;
-                        } else {
-                            final String[] imageIds = externalImageId.split(DELIMITER);
-                            if (imageIds.length > 1) {
-                                title = imageIds[0];
-                                detectedFaceType = FaceType.fromString(imageIds[1]);
-                            } else {
-                                title = "No prefix";
-                                detectedFaceType = FaceType.NOT_RECOGNIZED;
-                            }
-                        }
-                        log.debug("Number of matched faces for the detected face {}", faceSearchOutput.getMatchedFaceList().size());
-                    } else {
+            for(RekognizedOutput.FaceSearchOutput faceSearchOutput : rekognizedOutput.getFaceSearchOutputs()) {
+                log.debug("Face search output {}", faceSearchOutput);
+                FaceType detectedFaceType;
+                String title;
+                if (!faceSearchOutput.getMatchedFaceList().isEmpty()) {
+                    MatchedFace matchedFace = (MatchedFace)faceSearchOutput.getMatchedFaceList().get(0);
+                    log.debug("Matched face {}", matchedFace);
+                    String externalImageId = matchedFace.getFace().getExternalImageId();
+                    log.debug("External image id {}", externalImageId);
+                    if (externalImageId == null) {
+                        title = matchedFace.getFace().getConfidence() + "";
+                        log.debug("Title {}", title);
                         detectedFaceType = FaceType.NOT_RECOGNIZED;
-                        title = "Not recognized";
+                        log.debug("Detected face type {}", detectedFaceType);
+                    } else {
+                        String[] imageIds = externalImageId.split("-");
+                        if (imageIds.length > 1) {
+                            title = imageIds[0];
+                            log.debug("Title {}", title);
+                            detectedFaceType = FaceType.fromString(imageIds[1]);
+                            log.debug("Detected face type {}", detectedFaceType);
+                        } else {
+                            title = "No prefix";
+                            log.debug("Title {}", title);
+                            detectedFaceType = FaceType.NOT_RECOGNIZED;
+                            log.debug("Detected face type {}", detectedFaceType);
+                        }
                     }
-                    drawFaces(g2, width, height, faceSearchOutput.getDetectedFace().getBoundingBox(),
-                            title, detectedFaceType.getColor());
+
+                    log.debug("Number of matched faces for the detected face {}", faceSearchOutput.getMatchedFaceList().size());
+                } else {
+                    detectedFaceType = FaceType.NOT_RECOGNIZED;
+                    log.debug("Detected face type {}", detectedFaceType);
+                    title = "Not recognized";
+                    log.debug("Detected face type {}", detectedFaceType);
                 }
+
+                log.debug("DrawFaces g2 {} width {} height {} faceSearchOutput-getBoundingBox {} title {} detectedFaceType {}", new Object[]{g2, width, height, faceSearchOutput.getDetectedFace().getBoundingBox(), title, detectedFaceType.getColor()});
+                this.drawFaces(g2, width, height, faceSearchOutput.getDetectedFace().getBoundingBox(), title, detectedFaceType.getColor());
             }
         }
+
     }
 
-    private void drawFaces(final Graphics2D g2, final int width, final int height,
-                                  final BoundingBox boundingBox, final String personName, final Color color) {
-        final Color c = g2.getColor();
-
+    private void drawFaces(Graphics2D g2, int width, int height, BoundingBox boundingBox, String personName, Color color) {
+        log.debug(" drawFaceTitle width {} height {} boudingBox {} personName {} color {}", width, height, boundingBox, personName, color);
+        Color c = g2.getColor();
         g2.setColor(color);
-        // Draw bounding box
-        drawBoundingBox(g2, width, height, boundingBox);
-
-        // Draw title
-        drawFaceTitle(g2, width, height, boundingBox, personName);
+        this.drawBoundingBox(g2, width, height, boundingBox);
+        this.drawFaceTitle(g2, width, height, boundingBox, personName);
         g2.setColor(c);
     }
 
-    private void drawFaceTitle(final Graphics2D g2, final int width, final int height,
-                                      final BoundingBox boundingBox, final String personName) {
-        final int left = (int) (boundingBox.getLeft() * width);
-        final int top = (int) (boundingBox.getTop() * height);
+    private void drawFaceTitle(Graphics2D g2, int width, int height, BoundingBox boundingBox, String personName) {
+        log.debug(" drawFaceTitle width {} height {} boudingBox {} personName {}", width, height, boundingBox, personName);
+        int left = (int)(boundingBox.getLeft() * (double)width);
+        int top = (int)(boundingBox.getTop() * (double)height);
         g2.drawString(personName, left, top);
     }
 
-    private void drawBoundingBox(final Graphics2D g2, final int width, final int height,
-                                 final BoundingBox boundingBox) {
-        final int left = (int) (boundingBox.getLeft() * width);
-        final int top = (int) (boundingBox.getTop() * height);
-        final int bbWidth = (int) (boundingBox.getWidth() * width);
-        final int bbHeight = (int) (boundingBox.getHeight() * height);
-
-        // Draw bounding box
+    private void drawBoundingBox(Graphics2D g2, int width, int height, BoundingBox boundingBox) {
+        log.debug(" drawBoundingBox width {} height {} boudingBox {}", width, height, boundingBox);
+        int left = (int)(boundingBox.getLeft() * (double)width);
+        int top = (int)(boundingBox.getTop() * (double)height);
+        int bbWidth = (int)(boundingBox.getWidth() * (double)width);
+        int bbHeight = (int)(boundingBox.getHeight() * (double)height);
         g2.drawRect(left, top, bbWidth, bbHeight);
     }
 
-    public void setImage(final BufferedImage bufferedImage, final RekognizedOutput rekognizedOutput) {
+    public void setImage(BufferedImage bufferedImage, RekognizedOutput rekognizedOutput) {
         this.image = bufferedImage;
-        processRekognitionOutput(image.createGraphics(), image.getWidth(), image.getHeight(), rekognizedOutput);
-        repaint();
+        log.debug("Image {}", bufferedImage);
+        log.debug("RekognizedOutput {}", rekognizedOutput);
+        this.processRekognitionOutput(this.image.createGraphics(), this.image.getWidth(), this.image.getHeight(), rekognizedOutput);
+        this.repaint();
     }
 }
