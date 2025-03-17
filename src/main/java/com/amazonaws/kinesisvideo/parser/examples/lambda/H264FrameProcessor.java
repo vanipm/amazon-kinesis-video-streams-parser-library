f272409a5c68c862c39c0b5680d152f56c84862e
vanipm <waveam.vp@gmail.com>
2025-03-17 14:44:22 -0400

    Update H264FrameProcessor.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/H264FrameProcessor.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/H264FrameProcessor.java
@@ -28,25 +28,22 @@ import com.amazonaws.kinesisvideo.parser.utilities.H264FrameDecoder;
 import com.amazonaws.kinesisvideo.parser.utilities.H264FrameEncoder;
 import com.amazonaws.kinesisvideo.parser.utilities.MkvTrackMetadata;
 import com.amazonaws.kinesisvideo.parser.utilities.ProducerStreamUtil;
-import com.amazonaws.kinesisvideo.producer.StreamInfo;
+import com.amazonaws.kinesisvideo.producer.StreamInfo.NalAdaptationFlags;
 import com.amazonaws.regions.Regions;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
-
+import com.google.common.base.Preconditions;
 import java.awt.image.BufferedImage;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static com.google.common.base.Preconditions.checkState;
-
-@Slf4j
 public class H264FrameProcessor implements FrameVisitor.FrameProcessor {
-
+    private static final Logger log = LoggerFactory.getLogger(H264FrameProcessor.class);
     private static final int VIDEO_TRACK_NO = 1;
     private static final int MILLIS_IN_SEC = 1000;
     private static final int OFFSET_DELTA_THRESHOLD = 10;
-
-    private final BoundingBoxImagePanel boundingBoxImagePanel;
+    private final BoundingBoxImagePanel boundingBoxImagePanel = new BoundingBoxImagePanel();
     private final Regions regionName;
     private RekognizedOutput currentRekognizedOutput = null;
     private H264FrameEncoder h264Encoder;
@@ -56,213 +53,154 @@ public class H264FrameProcessor implements FrameVisitor.FrameProcessor {
     private boolean isEncoderInitialized = false;
     private final AWSCredentialsProvider credentialsProvider;
     private final String outputKvsStreamName;
-    @Setter
     private List<RekognizedOutput> rekognizedOutputs;
-    @Setter
     private int frameBitRate = 1024;
     private int frameNo = 0;
     private int currentWidth = 0;
     private int currentHeight = 0;
     private long keyFrameTimecode;
 
-    private H264FrameProcessor(final AWSCredentialsProvider credentialsProvider,
-                               final String outputKvsStreamName,
-                               final Regions regionName) {
-        this.boundingBoxImagePanel = new BoundingBoxImagePanel();
+    private H264FrameProcessor(AWSCredentialsProvider credentialsProvider, String outputKvsStreamName, Regions regionName) {
         this.credentialsProvider = credentialsProvider;
         this.outputKvsStreamName = outputKvsStreamName;
         this.regionName = regionName;
         this.h264Decoder = new H264FrameDecoder();
     }
 
-    private void initializeKinesisVideoProducer(final int width, final int height, final byte[] cpd) {
+    private void initializeKinesisVideoProducer(int width, int height, byte[] cpd) {
         try {
-            log.info("Initializing KVS Producer with stream name {} and region : {}",
-                    outputKvsStreamName, regionName);
-            final KinesisVideoClient kinesisVideoClient = KinesisVideoJavaClientFactory
-                    .createKinesisVideoClient(regionName, credentialsProvider);
+            log.info("Initializing KVS Producer with stream name {} and region : {}", this.outputKvsStreamName, this.regionName);
+            KinesisVideoClient kinesisVideoClient = KinesisVideoJavaClientFactory.createKinesisVideoClient(this.regionName, this.credentialsProvider);
             log.info("CameraMediaSourceConfiguration");
-            final CameraMediaSourceConfiguration configuration =
-                    new CameraMediaSourceConfiguration.Builder()
-                            .withFrameRate(30)
-                            .withRetentionPeriodInHours(1)
-                            .withCameraId("/dev/video0")
-                            .withIsEncoderHardwareAccelerated(false)
-                            .withEncodingMimeType("video/avc")
-                            .withNalAdaptationFlags(StreamInfo.NalAdaptationFlags.NAL_ADAPTATION_ANNEXB_NALS)
-                            .withIsAbsoluteTimecode(true)
-                            .withEncodingBitRate(200000)
-                            .withHorizontalResolution(width)
-                            .withVerticalResolution(height)
-                            .withCodecPrivateData(cpd)
-                            .build();
-            this.KVSMediaSource = new KVSMediaSource(
-                    ProducerStreamUtil.toStreamInfo(outputKvsStreamName, configuration));
+            CameraMediaSourceConfiguration configuration = (new CameraMediaSourceConfiguration.Builder()).withFrameRate(30).withRetentionPeriodInHours(1).withCameraId("/dev/video0").withIsEncoderHardwareAccelerated(false).withEncodingMimeType("video/avc").withNalAdaptationFlags(NalAdaptationFlags.NAL_ADAPTATION_ANNEXB_NALS).withIsAbsoluteTimecode(true).withEncodingBitRate(200000).withHorizontalResolution(width).withVerticalResolution(height).withCodecPrivateData(cpd).build();
+            this.KVSMediaSource = new KVSMediaSource(ProducerStreamUtil.toStreamInfo(this.outputKvsStreamName, configuration));
             this.KVSMediaSource.configure(configuration);
-            // register media source with Kinesis Video Client
-            kinesisVideoClient.registerMediaSource(KVSMediaSource);
-        } catch (final KinesisVideoException e) {
+            kinesisVideoClient.registerMediaSource(this.KVSMediaSource);
+        } catch (KinesisVideoException e) {
             log.error("Exception while initialize KVS Producer !", e);
         }
+
     }
 
     public void resetEncoder() {
-
-        // Reset frame count for this fragment
         if (this.isEncoderInitialized) {
             this.frameNo = 0;
-            this.h264Encoder.setFrameNumber(frameNo);
-
+            this.h264Encoder.setFrameNumber(this.frameNo);
         } else {
             throw new IllegalStateException("Encoder not initialized !");
         }
     }
 
-    public static H264FrameProcessor create(final AWSCredentialsProvider credentialsProvider,
-                                            final String rekognizedStreamName,
-                                            final Regions regionName) {
+    public static H264FrameProcessor create(AWSCredentialsProvider credentialsProvider, String rekognizedStreamName, Regions regionName) {
         return new H264FrameProcessor(credentialsProvider, rekognizedStreamName, regionName);
     }
 
-    /**
-     * Process Rekognized outputs for each rekognized output. For each kinesis event record i.e for each
-     * fragment number create a call getMediaForFragmentList, parse fragments, decode frame, draw bounding box,
-     * encode frame, call KVS PutFrame.
-     */
-    @Override
-    public void process(final Frame frame, final MkvTrackMetadata trackMetadata,
-                        final Optional<FragmentMetadata> fragmentMetadata) throws FrameProcessException {
-        if (rekognizedOutputs != null) {
-            // Process only for video frames
-            if (frame.getTrackNumber() == VIDEO_TRACK_NO) {
-                checkState(trackMetadata.getPixelWidth().isPresent() && trackMetadata.getPixelHeight().isPresent(),
-                        "Missing video resolution in track metadata !");
-                checkState(fragmentMetadata.isPresent(), "FragmentMetadata should be present !");
-
-                // Decode H264 frame
-                final BufferedImage decodedFrame = h264Decoder.decodeH264Frame(frame, trackMetadata);
-                log.debug("Decoded frame : {} with timecode : {} and fragment metadata : {}",
-                        frameNo, frame.getTimeCode(), fragmentMetadata.get());
-
-                // Get Rekognition results for this fragment number
-                final Optional<RekognizedOutput> rekognizedOutput = findRekognizedOutputForFrame(frame, fragmentMetadata);
-
-                // Render frame with bounding box
-                final BufferedImage compositeFrame = renderFrame(decodedFrame, rekognizedOutput);
-
-                // Encode to H264 frame
-                final EncodedFrame encodedH264Frame = encodeH264Frame(compositeFrame);
-                encodedH264Frame.setTimeCode(fragmentMetadata.get().getProducerSideTimestampMillis() + frame.getTimeCode());
-                log.debug("Encoded frame : {} with timecode : {}", frameNo, encodedH264Frame.getTimeCode());
-
-                // Call PutFrame for processed encodedFrame.
-                putFrame(encodedH264Frame, trackMetadata.getPixelWidth().get().intValue(),
-                        trackMetadata.getPixelHeight().get().intValue());
-                frameNo++;
+    public void process(Frame frame, MkvTrackMetadata trackMetadata, Optional<FragmentMetadata> fragmentMetadata) throws FrameProcessException {
+        log.debug("H264FrameProcessor-Processing frame: {}", frame);
+        if (this.rekognizedOutputs != null) {
+            if (frame.getTrackNumber() == 1L) {
+                Preconditions.checkState(trackMetadata.getPixelWidth().isPresent() && trackMetadata.getPixelHeight().isPresent(), "Missing video resolution in track metadata !");
+                Preconditions.checkState(fragmentMetadata.isPresent(), "FragmentMetadata should be present !");
+                BufferedImage decodedFrame = this.h264Decoder.decodeH264Frame(frame, trackMetadata);
+                log.debug("Decoded frame : {} with timecode : {} and fragment metadata : {}", new Object[]{this.frameNo, frame.getTimeCode(), fragmentMetadata.get()});
+                Optional<RekognizedOutput> rekognizedOutput = this.findRekognizedOutputForFrame(frame, fragmentMetadata);
+                BufferedImage compositeFrame = this.renderFrame(decodedFrame, rekognizedOutput);
+                EncodedFrame encodedH264Frame = this.encodeH264Frame(compositeFrame);
+                // encodedH264Frame.setTimeCode(((FragmentMetadata)fragmentMetadata.get()).getProducerSideTimestampMillis() + (long)frame.getTimeCode());
+                encodedH264Frame.setTimeCode( (long)frame.getTimeCode());
+                log.debug("Encoded frame : {} with timecode : {} ", this.frameNo, encodedH264Frame.getTimeCode());
+                this.putFrame(encodedH264Frame, ((BigInteger)trackMetadata.getPixelWidth().get()).intValue(), ((BigInteger)trackMetadata.getPixelHeight().get()).intValue());
+                ++this.frameNo;
             } else {
-              log.debug("Skipping audio frames !");
+                log.debug("Skipping audio frames !");
             }
         } else {
             log.warn("Rekognition output is empty");
         }
+
     }
 
-    private void putFrame(final EncodedFrame encodedH264Frame, final int width, final int height) {
-        if (!isKVSProducerInitialized) {
+    private void putFrame(EncodedFrame encodedH264Frame, int width, int height) {
+        if (!this.isKVSProducerInitialized) {
             log.info("Initializing JNI...");
-            initializeKinesisVideoProducer(width, height, encodedH264Frame.getCpd().array());
-            isKVSProducerInitialized = true;
+            this.initializeKinesisVideoProducer(width, height, encodedH264Frame.getCpd().array());
+            this.isKVSProducerInitialized = true;
         }
-        KVSMediaSource.putFrameData(encodedH264Frame);
-        log.debug("PutFrame successful for frame no : {}", frameNo);
+
+        this.KVSMediaSource.putFrameData(encodedH264Frame);
+        log.debug("PutFrame successful for frame no : {}", this.frameNo);
     }
 
-    private EncodedFrame encodeH264Frame(final BufferedImage bufferedImage) {
+    private EncodedFrame encodeH264Frame(BufferedImage bufferedImage) {
         try {
-            initializeEncoder(bufferedImage);
-            return h264Encoder.encodeFrame(bufferedImage);
-        } catch (final Exception e) {
+            this.initializeEncoder(bufferedImage);
+            return this.h264Encoder.encodeFrame(bufferedImage);
+        } catch (Exception e) {
             throw new RuntimeException("Unable to encode the bufferedImage !", e);
         }
     }
 
-    private void initializeEncoder(final BufferedImage bufferedImage) {
-        // Initialize the encoder if it's not initialized or if the current frame resolution changes from previous one.
-        if (!isEncoderInitialized ||
-                (currentWidth != bufferedImage.getWidth() || currentHeight != bufferedImage.getHeight())) {
-            this.h264Encoder = new H264FrameEncoder(bufferedImage.getWidth(), bufferedImage.getHeight(),
-                    frameBitRate);
+    private void initializeEncoder(BufferedImage bufferedImage) {
+        if (!this.isEncoderInitialized || this.currentWidth != bufferedImage.getWidth() || this.currentHeight != bufferedImage.getHeight()) {
+            this.h264Encoder = new H264FrameEncoder(bufferedImage.getWidth(), bufferedImage.getHeight(), this.frameBitRate);
             this.isEncoderInitialized = true;
             this.currentWidth = bufferedImage.getWidth();
             this.currentHeight = bufferedImage.getHeight();
         }
+
     }
 
-    private Optional<RekognizedOutput> findRekognizedOutputForFrame(final Frame frame,
-                                                                    final Optional<FragmentMetadata> fragmentMetadata) {
-
+    private Optional<RekognizedOutput> findRekognizedOutputForFrame(Frame frame, Optional<FragmentMetadata> fragmentMetadata) {
         Optional<RekognizedOutput> rekognizedOutput = Optional.empty();
         if (fragmentMetadata.isPresent()) {
-            final String fragmentNumber = fragmentMetadata.get().getFragmentNumberString();
-
-            // Currently Rekognition samples frames and calculates the frame offset from the fragment start time.
-            // So, in order to match with rekognition results, we have to compute the same frame offset from the
-            // beginning of the fragments.
+            String fragmentNumber = ((FragmentMetadata)fragmentMetadata.get()).getFragmentNumberString();
             if (frame.isKeyFrame()) {
-                keyFrameTimecode = frame.getTimeCode();
-                log.debug("Key frame timecode : {}", keyFrameTimecode);
+                this.keyFrameTimecode = (long)frame.getTimeCode();
+                log.debug("Key frame timecode : {}", this.keyFrameTimecode);
             }
-            final long frameOffset = (frame.getTimeCode() > keyFrameTimecode)
-                    ? frame.getTimeCode() - keyFrameTimecode : 0;
+
+            long frameOffset = (long)frame.getTimeCode() > this.keyFrameTimecode ? (long)frame.getTimeCode() - this.keyFrameTimecode : 0L;
             log.debug("Current Fragment Number : {} Computed Frame offset : {}", fragmentNumber, frameOffset);
             if (log.isDebugEnabled()) {
-                this.rekognizedOutputs
-                        .forEach(p -> log.debug("frameOffsetInSeconds from Rekognition : {}",
-                                p.getFrameOffsetInSeconds()));
+                this.rekognizedOutputs.forEach((p) -> log.debug("frameOffsetInSeconds from Rekognition : {}", p.getFrameOffsetInSeconds()));
             }
 
-            // Check whether the computed offset matches the rekognized output frame offset. Rekognition
-            // output is in seconds whereas the frame offset is calculated in milliseconds.
-            // NOTE: Rekognition frame offset doesn't exactly match with the computed offset below. So
-            // take the closest one possible within 10ms delta.
-            rekognizedOutput = this.rekognizedOutputs.stream()
-                    .filter(p -> isOffsetDeltaWithinThreshold(frameOffset, p))
-                    .findFirst();
-
-            // Remove from the index once the RekognizedOutput is processed. Else it would increase the memory
-            // footprint and blow up the JVM.
+            rekognizedOutput = this.rekognizedOutputs.stream().filter((p) -> this.isOffsetDeltaWithinThreshold(frameOffset, p)).findFirst();
             if (rekognizedOutput.isPresent()) {
-                log.debug("Computed offset matched with retrieved offset. Delta : {}",
-                        Math.abs(frameOffset - (rekognizedOutput.get().getFrameOffsetInSeconds() * MILLIS_IN_SEC)));
-
+                log.debug("Computed offset matched with retrieved offset. Delta : {}", Math.abs((double)frameOffset - ((RekognizedOutput)rekognizedOutput.get()).getFrameOffsetInSeconds() * (double)1000.0F));
                 if (this.rekognizedOutputs.isEmpty()) {
                     log.debug("All frames processed for this fragment number : {}", fragmentNumber);
                 }
             }
         }
+
         return rekognizedOutput;
     }
 
-
-
-    private boolean isOffsetDeltaWithinThreshold(final long frameOffset, final RekognizedOutput output) {
-        return Math.abs(frameOffset - (output.getFrameOffsetInSeconds() * MILLIS_IN_SEC)) <= OFFSET_DELTA_THRESHOLD;
+    private boolean isOffsetDeltaWithinThreshold(long frameOffset, RekognizedOutput output) {
+        return Math.abs((double)frameOffset - output.getFrameOffsetInSeconds() * (double)1000.0F) <= (double)10.0F;
     }
 
-    @SuppressWarnings("Duplicates")
-    private BufferedImage renderFrame(final BufferedImage bufferedImage, final Optional<RekognizedOutput> rekognizedOutput) {
+    private BufferedImage renderFrame(BufferedImage bufferedImage, Optional<RekognizedOutput> rekognizedOutput) {
         if (rekognizedOutput.isPresent()) {
             log.debug("Rendering Rekognized sampled frame...");
-            boundingBoxImagePanel.processRekognitionOutput(bufferedImage.createGraphics(), bufferedImage.getWidth(),
-                    bufferedImage.getHeight(), rekognizedOutput.get());
-            currentRekognizedOutput = rekognizedOutput.get();
-        } else if (currentRekognizedOutput != null) {
+            this.boundingBoxImagePanel.processRekognitionOutput(bufferedImage.createGraphics(), bufferedImage.getWidth(), bufferedImage.getHeight(), (RekognizedOutput)rekognizedOutput.get());
+            this.currentRekognizedOutput = (RekognizedOutput)rekognizedOutput.get();
+        } else if (this.currentRekognizedOutput != null) {
             log.debug("Rendering non-sampled frame with previous rekognized results...");
-            boundingBoxImagePanel.processRekognitionOutput(bufferedImage.createGraphics(), bufferedImage.getWidth(),
-                    bufferedImage.getHeight(), currentRekognizedOutput);
+            this.boundingBoxImagePanel.processRekognitionOutput(bufferedImage.createGraphics(), bufferedImage.getWidth(), bufferedImage.getHeight(), this.currentRekognizedOutput);
         } else {
             log.debug("Rendering frame without any rekognized results...");
         }
+
         return bufferedImage;
+    }
+
+    public void setRekognizedOutputs(List<RekognizedOutput> rekognizedOutputs) {
+        this.rekognizedOutputs = rekognizedOutputs;
+    }
+
+    public void setFrameBitRate(int frameBitRate) {
+        this.frameBitRate = frameBitRate;
     }
 }
