89d88cd0b1e7a37d87025dd7e2de681ea55be66b
vanipm <waveam.vp@gmail.com>
2025-03-18 02:17:46 -0400

    Update KVSMediaSource.java

--- src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/KVSMediaSource.java
+++ src/main/java/com/amazonaws/kinesisvideo/parser/examples/lambda/KVSMediaSource.java
@@ -82,7 +82,7 @@ public class KVSMediaSource implements MediaSource {
         log.debug("putFrameData : {} producerSideTimeStampMillis {} serverSideTimeStampMillis {} ", new Object[]{encodedFrame, encodedFrame.getProducerSideTimeStampMillis(), encodedFrame.getServerSideTimeStampMillis()});
         int flags = encodedFrame.isKeyFrame() ? 1 : 0;
         if (encodedFrame.getByteBuffer() != null) {
-            long startTime = System.nanoTime() / 100L;
+            long startTime = System.currentTimeMillis() * 10000L;
             KinesisVideoFrame frame = new KinesisVideoFrame(this.frameIndex++, flags, startTime, startTime, 200000L, encodedFrame.getByteBuffer());
             if (frame.getSize() == 0) {
                 return;
